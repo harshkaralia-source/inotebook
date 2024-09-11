e2faab516fb4b04d42dccdc9e1ba6bcd337b17e4
harshkaralia-source <harshkaralia.dev@gmail.com>
2024-09-11 17:53:31 +0530

    added validation using express validator

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,19 +1,33 @@
 import express from 'express'
 import UserSchema from '../models/User.js'
+import { body, validationResult } from 'express-validator'
 
 export const UserRouter = express.Router()
 
 // Route 1 - creating a user "api/user/createuser"
-UserRouter.post('/createuser', (req, res) => {
-    // log the request body to the console
-    console.log(req.body)
+UserRouter.post('/createuser', [
 
-    // create a new user object using the request data
-    const user = new UserSchema(req.body)
+    // validating the fields
+    body('email', 'Enter a valid name').isEmail(),
+    body('name', 'Enter a valid email').isLength({ min: 3 }),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 })
+], (req, res) => {
 
-    // save the new user object to db
-    user.save()
+    // checking for validation errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
 
-    // sending the request body as a response
-    res.send(req.body)
+    // getting the user schema/model
+    const user = UserSchema
+
+    user.create({
+
+        // setting the fields from request body
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user)) // sending the created user as json response
+        .catch(error => console.log(error)) // logging any potential errors
 })
