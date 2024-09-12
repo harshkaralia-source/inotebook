f853f1bfd3ec8f81b7b525fcd86aa66d08245ab7
harshkaralia-source <harshkaralia.dev@gmail.com>
2024-09-12 20:21:33 +0530

    added password hashing

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import UserSchema from '../models/User.js'
 import { body, validationResult } from 'express-validator'
+import bcrypt from 'bcryptjs'
 
 export const UserRouter = express.Router()
 
@@ -11,23 +12,35 @@ UserRouter.post('/createuser', [
     body('email', 'Enter a valid name').isEmail(),
     body('name', 'Enter a valid email').isLength({ min: 3 }),
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
 
-    // checking for validation errors
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+    try {
+        // checking for validation errors
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        // getting the user schema/model
+        const user = UserSchema
+
+        // generating salt for password
+        const salt = await bcrypt.genSalt(10)
+
+        // hashing password
+        const securePassword = await bcrypt.hash(req.body.password, salt)
+
+        user.create({
+
+            // setting the fields from request body
+            name: req.body.name,
+            email: req.body.email,
+            password: securePassword
+        }).then(user => res.json(user)) // sending the created user as json response
+            .catch(error => console.log(error)) // logging any potential errors
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send('Some error occured')
     }
-
-    // getting the user schema/model
-    const user = UserSchema
-
-    user.create({
-
-        // setting the fields from request body
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user)) // sending the created user as json response
-        .catch(error => console.log(error)) // logging any potential errors
 })
